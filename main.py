c790029cc841f0c35a8a9d472f2ae5a57a444995
ebnyame <ebnyame001@st.ug.edu.gh>
2025-08-13 02:11:43 +0000

    Mini challenge for list

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+Mini Challenge
+You have this list:
+
+data = [5, 8, 12, 20]
+Change the second element (8) to 10.
+
+Add the number 25 at the end.
+
+Print the length of the list.
+'''
+
+data = [5, 8, 12, 20]
+
+data[1] = 10
+data.append(25)
+print(len(data))
