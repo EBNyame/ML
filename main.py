f3160c47b8ea185b253710f9e4ba9255e29149bf
ebnyame <ebnyame001@st.ug.edu.gh>
2025-08-13 02:36:35 +0000

    Task:
    Loop through the list.
    
    Print "Pass" if the score is ≥ 60, otherwise print "Fail".

--- main.py
+++ main.py
@@ -15,3 +15,27 @@ data = [5, 8, 12, 20]
 data[1] = 10
 data.append(25)
 print(len(data))
+
+
+
+'''
+Mini Challenge
+You have:
+
+
+scores = [80, 45, 67, 90, 55]
+Task:
+
+Loop through the list.
+
+Print "Pass" if the score is ≥ 60, otherwise print "Fail".
+
+'''
+
+scores = [80, 45, 67, 90, 55]
+
+for score in scores:
+    if score >= 60:
+        print("Pass")
+    else:
+        print("Fail")
