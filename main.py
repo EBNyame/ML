fcf7ceec44711682ed2dd794b360a9de8399a7b5
Exodus Blessed Nyame <exodusbnyame@gmail.com>
2025-08-15 16:58:28 +0000

    Numpy Array

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 '''
 Mini Challenge
 You have this list:
@@ -9,7 +10,7 @@ Add the number 25 at the end.
 
 Print the length of the list.
 '''
-
+print("---------------Array-------------------")
 data = [5, 8, 12, 20]
 
 data[1] = 10
@@ -31,7 +32,7 @@ Loop through the list.
 Print "Pass" if the score is ≥ 60, otherwise print "Fail".
 
 '''
-
+print("---------------Conditional Statements-------------------")
 scores = [80, 45, 67, 90, 55]
 
 for score in scores:
@@ -39,3 +40,33 @@ for score in scores:
         print("Pass")
     else:
         print("Fail")
+
+
+'''
+Mini Challenge
+
+Given:
+
+data = np.array([[5, 8, 10],
+                 [3, 6, 9],
+                 [4, 7, 11],
+                 [2, 5, 8]])
+
+
+Do the following:
+
+Print the second row.
+
+Print the first column.
+
+Print the sub-array containing rows 1–2 and columns 1–2.
+'''
+print("---------------Numpy array-------------------")
+data = np.array([[5, 8, 10],
+                 [3, 6, 9],
+                 [4, 7, 11],
+                 [2, 5, 8]])
+
+print(data[1])
+print(data[:, 0])
+print(data[1:3, 1:3])
